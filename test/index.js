60f3591d8d62731fa7592933f1e8d0f389f445f3
Andrew Morris <voltrevo@gmail.com>
2015-11-07 18:16:53 +1100

    Test basic propagation

--- test/index.js
+++ test/index.js
@@ -2,17 +2,38 @@ import assert from 'assert';
 import Ixonet from '../lib';
 
 describe('ixonet', function() {
-  it('should have a head with value 1', function() {
+  it('should have a head with value 1', () => {
     let ixonet = Ixonet();
 
     assert.equal(ixonet.head.value.curr, 1);
   });
 
-  it('should still have head with value 1 after a step', function() {
+  it('should still have head with value 1 after a step', () => {
     let ixonet = Ixonet();
 
     ixonet.step();
 
     assert.equal(ixonet.head.value.curr, 1);
+  });
+
+  it('should decay in propagation', () => {
+    let ixonet = Ixonet();
+
+    let head = ixonet.head;
+    let a = ixonet.Ixon();
+    let b = ixonet.Ixon();
+
+    ixonet.connect(head, head, a);
+    ixonet.connect(a, head, b);
+
+    ixonet.step();
+
+    assert.equal(a.value.curr, ixonet.decay);
+    assert.equal(b.value.curr, 0);
+
+    ixonet.step();
+
+    assert.equal(a.value.curr, ixonet.decay);
+    assert.equal(b.value.curr, ixonet.decay * ixonet.decay);
   });
 });
